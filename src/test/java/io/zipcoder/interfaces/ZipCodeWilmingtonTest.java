77ed0bc8276647699c9684166abfb7b8d9610156
Nirmala <nirmala.talari@gmail.com>
2019-03-07 22:22:38 -0500

    Learners

--- src/test/java/io/zipcoder/interfaces/ZipCodeWilmingtonTest.java
+++ src/test/java/io/zipcoder/interfaces/ZipCodeWilmingtonTest.java
@@ -8,10 +8,8 @@ import static org.junit.Assert.*;
 public class ZipCodeWilmingtonTest {
 
     @Test
-    public void hostLectureby() {
+    public void hostLectureByTeacherTest() {
         double noOfHrs = 100;
-        Long expectedInstId1 = 1L;
-        Long expectedInstId2 = 2L;
 
         Students studSingleton = Students.getInstance();
         ZipCodeWilmington singleton = ZipCodeWilmington.getInstance();
@@ -23,22 +21,55 @@ public class ZipCodeWilmingtonTest {
         studSingleton.add(stud1);
         studSingleton.add(stud2);
         studSingleton.add(stud3);
+        studSingleton.add(stud4);
 
-        Instructor instructor1 = new Instructor(expectedInstId1);
-        Instructor instructor2 = new Instructor(expectedInstId2);
+        Instructor instructor1 = new Instructor(1L);
+        //  Instructor instructor2 = new Instructor(2L);
         Student[] studArr = new Student[]{new Student(50L), new Student(51L),
                 new Student(52L), new Student(53L)};
 
         singleton.hostLecture(instructor1, noOfHrs);
-        singleton.hostLecture(instructor2, noOfHrs);
+//        singleton.hostLecture(instructor2, noOfHrs);
 
         double expectedHrs = 100 / studArr.length;
-        double actualHrs = studArr[1].getTotalStudyTime();
+        double actualHrs = stud1.getTotalStudyTime();
         Assert.assertEquals(expectedHrs, actualHrs, 0);
-        Assert.assertEquals(studArr[2].getTotalStudyTime(), studArr[3].getTotalStudyTime(), 0);
+        Assert.assertEquals(stud1.getTotalStudyTime(), stud4.getTotalStudyTime(), 0);
     }
 
     @Test
-    public void hostLecture1() {
+    public void hostLectureByTeacherIdTest() {
+        //Given
+        double noOfHrs = 100;
+        Student stud1 = new Student(50L);
+        Person stud2 = new Student(51L);
+        Student stud3 = new Student(52L);
+        Student stud4 = new Student(53L);
+        Instructor instructor1 = new Instructor(1L);
+        Instructor instructor2 = new Instructor(2L);
+        Students studSingleton = Students.getInstance();
+        Instructors instructorSingleton = Instructors.getInstance();
+        ZipCodeWilmington singleton = ZipCodeWilmington.getInstance();
+
+        studSingleton.add(stud1);
+        studSingleton.add(stud2);
+        studSingleton.add(stud3);
+        studSingleton.add(stud4);
+
+        instructorSingleton.add(instructor1);
+        instructorSingleton.add(instructor2);
+
+        //When
+        singleton.hostLecture(1L, noOfHrs);
+        singleton.hostLecture(2L, noOfHrs);
+
+        double expectedHrs = (100 / studSingleton.peopleList.size())*instructorSingleton.getCount();
+        double actualHrs = stud1.getTotalStudyTime();
+
+        //Then
+        Assert.assertEquals(expectedHrs, actualHrs, 0);
+        Assert.assertEquals(stud1.getTotalStudyTime(), stud4.getTotalStudyTime(), 0);
+
     }
+
 }
